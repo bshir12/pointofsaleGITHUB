00ab31a059b95d56a47c2db6b0f588ca049fba80
abdulqowi <93390507+abdulqowi@users.noreply.github.com>
2023-11-27 14:47:54 +0700

    mock service

--- src/test/java/com/pointofsale/controller/PembeliControllerTest.java
+++ src/test/java/com/pointofsale/controller/PembeliControllerTest.java
@@ -1,15 +1,20 @@
 package com.pointofsale.controller;
 
+import com.pointofsale.service.PembeliService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PembeliControllerTest {
     @InjectMocks
-    private PembeliController pembeliController
+    private PembeliController pembeliController;
+    @Mock
+    private PembeliService pembeliService;
+
 
     @BeforeEach
     void setUp() {
