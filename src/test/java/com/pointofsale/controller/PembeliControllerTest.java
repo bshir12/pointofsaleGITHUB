d31da987886399b3e4162d742d84247f8f637046
abdulqowi <93390507+abdulqowi@users.noreply.github.com>
2023-11-27 14:27:55 +0700

    dassa

---
+++ src/test/java/com/pointofsale/controller/PembeliControllerTest.java
@@ -0,0 +1,34 @@
+package com.pointofsale.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PembeliControllerTest {
+    @InjectMocks
+    private PembeliController pembeliController
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void getAllPembeli() {
+    }
+
+    @Test
+    void savePembeli() {
+    }
+
+    @Test
+    void updatePembeliById() {
+    }
+
+    @Test
+    void deletePembeliById() {
+    }
+}
