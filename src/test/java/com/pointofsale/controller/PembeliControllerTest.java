34dd8701f61b87bdb25060d61d0e001ab67f7e5d
abdulqowi <93390507+abdulqowi@users.noreply.github.com>
2023-11-27 14:36:09 +0700

    mockito

---
+++ src/test/java/com/pointofsale/controller/PembeliControllerTest.java
@@ -0,0 +1,30 @@
+package com.pointofsale.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PembeliControllerTest {
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void getAllPembeli() {
+    }
+
+    @Test
+    void savePembeli() {
+    }
+
+    @Test
+    void updatePembeliById() {
+    }
+
+    @Test
+    void deletePembeliById() {
+    }
+}
